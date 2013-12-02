d62731c07bb3a33e71d1564482883b3adb0063ef
Veselin Todorov <veselin@veselin.bg>
2013-12-02 09:58:56 +0200

    Initial implementation

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * External dependencies.
+ */
+
+var hydro = require('hydro');
+var Formatter = hydro.Formatter;
+
+/**
+ * Silent formatter.
+ *
+ * @constructor
+ */
+
+var Silent = Formatter.extend();
+
+/**
+ * Primary export.
+ */
+
+module.exports = Silent;

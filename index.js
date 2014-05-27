faf03ecc7dae886e55e7cd8aef1ae337c96182f5
Jake Rosoman <jkroso@gmail.com>
2014-05-27 22:10:59 +1200

    use new hydro api

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-/**
- * External dependencies.
- */
-
-var Formatter = require('hydro-formatter');
-
-/**
- * Silent formatter.
- *
- * @constructor
- */
-
-var Silent = Formatter.extend();
 
 /**
  * Primary export.
  */
 
-module.exports = Silent;
+module.exports = function(){};
